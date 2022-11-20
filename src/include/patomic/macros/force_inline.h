1dd2c43d9a4fbee90c086312d37f93e1c879c6d9
brian.kim <kbs1112@gmail.com>
2022-11-20 10:31:14 +0000

    force_inline.h (#1)

--- src/include/patomic/macros/force_inline.h
+++ src/include/patomic/macros/force_inline.h
@@ -4,18 +4,22 @@
 #include <patomic/patomic_config.h>
 
 #if PATOMIC_HAVE_MS_FORCEINLINE
+    #undef PATOMIC_FORCE_INLINE
     #define PATOMIC_FORCE_INLINE __forceinline
 #endif
 
 #if PATOMIC_HAVE_ALWAYS_INLINE_ATTR
+    #undef PATOMIC_FORCE_INLINE
     #define PATOMIC_FORCE_INLINE __attribute__((always_inline))
 #endif
 
 #if PATOMIC_HAVE_INLINE_ALWAYS_INLINE_ATTR
+    #undef PATOMIC_FORCE_INLINE
     #define PATOMIC_FORCE_INLINE inline __attribute__((always_inline))
 #endif
 
 #if PATOMIC_HAVE_GNU_INLINE_ALWAYS_INLINE_ATTR
+    #undef PATOMIC_FORCE_INLINE
     #define PATOMIC_FORCE_INLINE __inline__ __attribute__((always_inline))
 #endif
 
